abc706f9c98e6a11ce47d1e7012304f2e6c53090
Jovanovic Aleks <153079016+pylori-hummock@users.noreply.github.com>
2023-12-06 17:51:43 +0700

    Create password_generator.h

---
+++ password_generator.h
@@ -0,0 +1,15 @@
+// password_generator.h
+#ifndef PASSWORD_GENERATOR_H
+#define PASSWORD_GENERATOR_H
+
+#include <string>
+
+class PasswordGenerator {
+public:
+    std::string generatePassword(int length);
+    
+private:
+    // Add any private helper functions or variables here
+};
+
+#endif // PASSWORD_GENERATOR_H
